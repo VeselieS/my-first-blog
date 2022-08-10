70ca16efb3b1d1d2bc64bb4c283eace7111e827e
VeselieS <vadik.volkorezov@yandex.ru>
2022-08-10 19:31:44 +0900

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
-from .models import Post
-from django.shortcuts import render, get_object_or_404
+from .models import Post 
 from .forms import PostForm
 
 def post_list(request):
